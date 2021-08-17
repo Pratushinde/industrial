21f59f2a1bc8c9589791ceaa838361ebe2f3d6b5
Pratushinde <88925196+Pratushinde@users.noreply.github.com>
2021-08-17 09:45:34 +0530

    Create Jpr2.java

---
+++ Jpr2.java
@@ -0,0 +1,10 @@
+package com.Training;
+
+public class Jpr2 {
+
+	public static void main(String[] args) {
+		    {
+		        System.out.println(" \" can we print '\\' with System.out.println() statement? \" ");
+		    }
+
+	}
